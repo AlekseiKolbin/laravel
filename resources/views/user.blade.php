e7e80a44cf56bf1b9c50f874ff8570bc8f2a89ea
Aleksei <uayt.1998@mail.ru>
2020-10-05 11:06:57 +0300

    fix

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -53,6 +53,30 @@
 @else
 Пользователь №: {{$user->id}}
 @endif
+<div class="user_profile_mes_form">
+  <form method="post" action="{{ route('message', ['id' => $user->id])}}">
+    @csrf
+    <div class="form-group row">
+      <label for="inputEmail3" class="col-sm-2 col-form-label">Заголовок сообщения</label>
+      <div class="col-sm-10">
+        <input name="title" type="text" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" id="inputEmail3" placeholder="Заголовок сообщения">
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="inputPassword3" class="col-sm-2 col-form-label">Текст сообщения</label>
+      <div class="col-sm-10">
+        <input name="message" type="text" class="form-control{{ $errors->has('message') ? ' is-invalid' : '' }}" id="inputPassword3" placeholder="Текст сообщения">
+      </div>
+    </div>
+    <div class="form-group row">
+      <div class="col-sm-10">
+        <button type="submit" class="btn btn-primary">Отправить</button>
+      </div>
+    </div>
+    @if($errors->has('title')) <span class="help-block text-danger">{{ $errors->first('title') }}</span> @endif <br>
+    @if($errors->has('message')) <span class="help-block text-danger">{{ $errors->first('message') }}</span> @endif
+  </form>
+</div>
 @foreach($datas as $data)
 <div class="card">
   <div id="messages">
